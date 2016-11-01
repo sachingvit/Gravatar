d1e8c28f1702bfdf6fed0d1bd2b2c38e8a88c785
sachingvit <sachingvit@github.com>
2016-11-01 19:07:52 +0530

    update library and UI

--- js/gravatar.js
+++ js/gravatar.js
@@ -1,10 +1,10 @@
 /*!
- * Gravatar plugin v1.1 Beta
- * 
+ * Gravatar plugin v2.0 Beta
+ *
  * Copyright 2015, 2015 Open Source Foundation
  * Released under the MIT license
- * 
- * Date: 2015-10-09T06:14
+ * Author: Sachin Jain
+ * Date: 2016-11-01T06:14
  */
 
 "use strict";
@@ -12,32 +12,27 @@
     $.fn.avatar = function (options) {
         var options = $.extend({
             width: 300,
-            height: 200,
-            type:'round'
+            height: 200
         }, options);
-        
-        console.log(options);
+
         return $(this).each(function () {
             var display = '';
             var element = $(this).attr('data-avatar');
             var elementImage = ((typeof $(this).attr('data-image-src') !== typeof undefined && $(this).attr('data-image-src') !== false) ? true : false);
-            console.log('Element image: ' + elementImage);
             if ($(this).is('div') || $(this).is('span')) {
                 var strArr = element.split(' ');
-                
+
                 /* validate broken link */
                 var isBroken = false;
                 if (elementImage) {
-                    $(this).addClass('avatar-img '+options.type).attr('title',element).html('<img src="' + $(this).attr('data-image-src') + '" class="image-avatar">').addClass('avatar-A');
-                //                    $(this).addClass('avatar-' + display).html('<span class="text-avatar">'+display.toUpperCase()+'</span>').addClass('avatar-A');
+                    $(this).addClass('avatar-img').attr('title', element).html('<img src="' + $(this).attr('data-image-src') + '" class="image-avatar">').addClass('avatar-A');
                 }
                 if (strArr.length) {
-                    
+
                     display = ((strArr[0] != undefined)?strArr[0].charAt(0):'')+ ((strArr[1] != undefined)?strArr[1].charAt(0):'');
-//                    $(this).addClass('avatar-' + display[0]).attr('title', element)
-//                    .html($(this).html() + '<span style="display:' + (elementImage ? "none" : "block") + '" class="text-avatar">' + display.toUpperCase() + '</span>')
-//                    .addClass('avatar-A');
-                    $(this).addClass('avatar-' + display[0]+ ' '+ options.type).attr('title',element).html($(this).html() + '<span class="text-avatar">' + display.toUpperCase() + '</span>');
+
+                    $(this).addClass('avatar-' + display[0]).attr('title', element).html($(this).html() + '<span style="display:' + (elementImage ? "none" : "block") + '" class="text-avatar">' + display.toUpperCase() + '</span>').addClass('avatar-A');
+
                     imageValidator();
                 }
             }
@@ -47,22 +42,6 @@
 function  imageValidator() {
     $(document).find("img").error(function () {
         $(this).hide().next('span').show();
-        console.log($(this).hide().next('span').show());
-    //    $(this).unbind("error").attr("src", "broken.gif");
+//    $(this).unbind("error").attr("src", "broken.gif");
     });
 }
-/*"use strict";
- (function () {
- $.fn.avatar = function () {
- $(this).each(function () {
- var element = $(this).attr('data-avatar');
- if (element.length) {
- var charArray = element.split("");
- if (charArray[0] != undefined) {
- $(this).addClass('avatar-' + charArray[0]).text(charArray[0]);
- }
- }
- });
- };
- }(jQuery));
- */
